0f16d38e84f30799f98cfa43a725385b118030f1
DemonicAnimal <devt23124@wainuiomatahigh.school.nz>
2025-04-01 12:37:58 +1300

    Update main.py
    Doesn't work as hoped

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 import time
+bool_choice = True
+str_userChoice =''
+
 print(" Welcome to my lord of the rings quiz! ")
+
 time.sleep(1.5)
 print(" type either a, b, c, d, to awnser a question ") 
+
 time.sleep(2.5)
 print(" \n Question 1! ")
 time.sleep(0.5)
 print(" what is the first lord of the rings movie? ")
+
 time.sleep(2.0)
 print(" \n a) The Lord of the Rings: The Fellowship of the Ring ")
+
 time.sleep(0.5)
 print(" \n b) The Lord of the Rings: The Return of the King ")
+
 time.sleep(0.5)
 print(" \n c) The Lord of the Rings: The Two Towers")
+
 time.sleep(0.5)
 print(" \n d) The Lord of the Rings: The Great War of the Ring ")
+
+while bool_choice is True:
+
+    str_userChoice == input(" what leter will you pick a,b,c,d, ")
+
+    if str_userChoice == 'a':  
+        print('Correct') 
+        bool_choice = False 
+    
+    elif str_userChoice == 'b':
+        print('Incorrect') 
+        bool_choice = False 
+    
+    elif str_userChoice == 'c':
+        print('Incorrect') 
+        bool_choice = False 
+    
+    elif str_userChoice == 'd':
+        print('Incorrect') 
+        bool_choice = False 
+    
+    else:
+        print('Invalid') 
